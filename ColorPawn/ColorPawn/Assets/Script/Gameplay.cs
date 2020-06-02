fe294656c08dd3670f5e61f6735c8f525a9a3ebe
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-06-02 03:19:39 +0300

    ColorPawn
    02/06/2020
    
    03.18

--- ColorPawn/ColorPawn/Assets/Script/Gameplay.cs
+++ ColorPawn/ColorPawn/Assets/Script/Gameplay.cs
@@ -48,7 +48,9 @@ public class Gameplay : MonoBehaviour
     public Button controlButton, leaveButton;
     RaycastHit selectedRegion, selectedPawn;
     List<GameObject> livePawn = new List<GameObject>();
+    GameObject selectedPawnSymbol, selectedRegionSymbol;
     float redWinCounter = 0, yellowdWinCounter = 0, greendWinCounter = 0, bluedWinCounter = 0, orangedWinCounter = 0, pinkdWinCounter = 0;
+    bool flagSelected = false; /* Pawn secilip secilmedigini tutar */
 
     void LivePawn() /* oyundaki kazanilmamis piyonlari tutan listeyi hazirlar */
     {
@@ -144,17 +146,19 @@ public class Gameplay : MonoBehaviour
 
         if (Input.touchCount == 1 && Physics.Raycast(rayArea, out hitArea)) /* Dokunulan objenin ne oldugunu tanimlar */
         {
-            if (hitArea.transform.name.Contains("region") && RegionEmptyControl(hitArea)) /* secilen objenin region olup olmadigini anlamaya calisir ve ustunde pawn var mi kontrol eder */
-            {
-                selectedRegion = hitArea;
-                //Debug.Log("selectedRegion: " + selectedRegion.transform.name);
-            }
             if (hitArea.transform.name.Contains("pawn"))
             {
                 selectedPawn = hitArea;
+                selectedPawnSymbol.transform.position = new Vector3(selectedPawn.transform.position.x, 0.07f, selectedPawn.transform.position.z + 0.1f); /* Halkayi isaretler */
+                flagSelected = true; /* Pawn secilince true ya donusur */
                 //Debug.Log("selectedPawn: " + selectedPawn.transform.name);
             }
-
+            if (hitArea.transform.name.Contains("region") && RegionEmptyControl(hitArea) && flagSelected) /* secilen objenin region olup olmadigini anlamaya calisir ve ustunde pawn var mi kontrol eder */
+            {
+                selectedRegion = hitArea;
+                selectedRegionSymbol.transform.position = new Vector3(selectedRegion.transform.position.x, 0.07f, selectedRegion.transform.position.z + 0.1f); /* X'i isaretler */
+                //Debug.Log("selectedRegion: " + selectedRegion.transform.name);
+            }
         }
 
         leaveButton.onClick.AddListener(PushLeave); 
@@ -162,6 +166,9 @@ public class Gameplay : MonoBehaviour
         {
             selectedRegion = new RaycastHit();
             selectedPawn = new RaycastHit();
+            selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f);
+            selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f);
+            flagSelected = false; /* Secilen pawn unutuldugundan false ye donusur */
         }
 
         controlButton.onClick.AddListener(PushControl);
@@ -176,9 +183,16 @@ public class Gameplay : MonoBehaviour
                     selectedPawn.transform.position = new Vector3(selectedRegion.transform.position.x, selectedPawn.transform.position.y, selectedRegion.transform.position.z);
                     selectedRegion = new RaycastHit();
                     selectedPawn = new RaycastHit();
+                    selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
+                    selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
+                    flagSelected = false; /* Yeni bir pawn gerektiginden false ye donusur */
                 }
                 else
-                    return;
+                {
+                    selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
+                    selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
+                    flagSelected = false; /* Islemi hatali yaptıgından false ye donusur */
+                }
             }
             else
                 return;
@@ -193,6 +207,8 @@ public class Gameplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        selectedPawnSymbol = GameObject.Find("Circle"); /* Halkayi bulur */
+        selectedRegionSymbol = GameObject.Find("XSymbol"); /* X' i bulur */
         LivePawn();
         //A();
     }
