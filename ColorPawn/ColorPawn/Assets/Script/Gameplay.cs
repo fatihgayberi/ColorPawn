4cfa6dbdcd4b485083f0df9cf13fe20b675a8ec8
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-06-08 22:22:11 +0300

    ColorPawn
    22.21

--- ColorPawn/ColorPawn/Assets/Script/Gameplay.cs
+++ ColorPawn/ColorPawn/Assets/Script/Gameplay.cs
@@ -47,6 +47,7 @@ public class Gameplay : MonoBehaviour
 {
     public Button controlButton, leaveButton;
     RaycastHit selectedRegion, selectedPawn;
+    List<RaycastHit> selectedRegionList = new List<RaycastHit>();
     List<GameObject> livePawn = new List<GameObject>();
     GameObject selectedPawnSymbol, selectedRegionSymbol;
     float redWinCounter = 0, yellowdWinCounter = 0, greendWinCounter = 0, bluedWinCounter = 0, orangedWinCounter = 0, pinkdWinCounter = 0;
@@ -151,12 +152,14 @@ public class Gameplay : MonoBehaviour
                 selectedPawn = hitArea;
                 selectedPawnSymbol.transform.position = new Vector3(selectedPawn.transform.position.x, 0.07f, selectedPawn.transform.position.z + 0.1f); /* Halkayi isaretler */
                 flagSelected = true; /* Pawn secilince true ya donusur */
+                selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* yeniden secimleri yapmak isterse secilen bolge ustundeki X'i kaldirir */
                 //Debug.Log("selectedPawn: " + selectedPawn.transform.name);
             }
             if (hitArea.transform.name.Contains("region") && RegionEmptyControl(hitArea) && flagSelected) /* secilen objenin region olup olmadigini anlamaya calisir ve ustunde pawn var mi kontrol eder */
             {
-                selectedRegion = hitArea;
-                selectedRegionSymbol.transform.position = new Vector3(selectedRegion.transform.position.x, 0.07f, selectedRegion.transform.position.z + 0.1f); /* X'i isaretler */
+                //selectedRegion = hitArea;
+                selectedRegionList.Add(hitArea);
+                selectedRegionSymbol.transform.position = new Vector3(hitArea.transform.position.x, 0.07f, hitArea.transform.position.z + 0.1f); /* X'i isaretler */
                 //Debug.Log("selectedRegion: " + selectedRegion.transform.name);
             }
         }
@@ -164,7 +167,8 @@ public class Gameplay : MonoBehaviour
         leaveButton.onClick.AddListener(PushLeave); 
         void PushLeave() /* leaveButton a tiklandiginda tutlan degerleri sifirlar. */
         {
-            selectedRegion = new RaycastHit();
+            selectedRegionList.Clear();
+            //selectedRegion = new RaycastHit();
             selectedPawn = new RaycastHit();
             selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f);
             selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f);
@@ -174,28 +178,32 @@ public class Gameplay : MonoBehaviour
         controlButton.onClick.AddListener(PushControl);
         void PushControl() /* controlButton a tiklandiginda gerekli sartlarin saglanip saglanmadiginin kontrolu saglanir. */
         {
-            if (selectedPawn.collider != null && selectedRegion.collider != null)
+            for (int i = 0; i < selectedRegionList.Count; i++)
             {
-                if (((Mathf.Abs(selectedPawn.transform.position.x - selectedRegion.transform.position.x) == 2f && selectedPawn.transform.position.z == selectedRegion.transform.position.z)
-                    || (selectedPawn.transform.position.x == selectedRegion.transform.position.x && Mathf.Abs(selectedPawn.transform.position.z - selectedRegion.transform.position.z) == 2f)) 
-                    && PawnWin(selectedRegion, selectedPawn))
+                if (selectedPawn.collider != null && selectedRegionList[i].collider != null)
                 {
-                    selectedPawn.transform.position = new Vector3(selectedRegion.transform.position.x, selectedPawn.transform.position.y, selectedRegion.transform.position.z);
-                    selectedRegion = new RaycastHit();
-                    selectedPawn = new RaycastHit();
-                    selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
-                    selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
-                    flagSelected = false; /* Yeni bir pawn gerektiginden false ye donusur */
+                    if (((Mathf.Abs(selectedPawn.transform.position.x - selectedRegionList[i].transform.position.x) == 2f && selectedPawn.transform.position.z == selectedRegionList[i].transform.position.z)
+                        || (selectedPawn.transform.position.x == selectedRegionList[i].transform.position.x && Mathf.Abs(selectedPawn.transform.position.z - selectedRegionList[i].transform.position.z) == 2f))
+                        && PawnWin(selectedRegionList[i], selectedPawn))
+                    {
+                        selectedPawn.transform.position = new Vector3(selectedRegionList[i].transform.position.x, selectedPawn.transform.position.y, selectedRegionList[i].transform.position.z);
+                        //selectedRegion = new RaycastHit();
+                        selectedPawn = new RaycastHit();
+                        selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
+                        selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
+                        flagSelected = false; /* Yeni bir pawn gerektiginden false ye donusur */
+                    }
+                    else
+                    {
+                        selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
+                        selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
+                        flagSelected = false; /* Islemi hatali yaptıgından false ye donusur */
+                    }
                 }
                 else
-                {
-                    selectedPawnSymbol.transform.position = new Vector3(selectedPawnSymbol.transform.position.x, -0.68f, selectedPawnSymbol.transform.position.z + 0.1f); /* Halkayi gizler */
-                    selectedRegionSymbol.transform.position = new Vector3(selectedRegionSymbol.transform.position.x, -0.18f, selectedRegionSymbol.transform.position.z + 0.1f); /* X'i gizler */
-                    flagSelected = false; /* Islemi hatali yaptıgından false ye donusur */
-                }
+                    return;
             }
-            else
-                return;
+            selectedRegionList.Clear();
         }
     }
    
